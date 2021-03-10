4df2b7d8dbab893e840a3ac07b1838f937199cba
Jesse Ruiz <j.ruiz@fetchrewards.com>
2021-03-10 11:13:48 -0800

    Built out classificationRequest property.

--- mlt-materials-editions-2.0/02-image-classification/projects/starter/HealthySnacks/Snacks/ViewController.swift
+++ mlt-materials-editions-2.0/02-image-classification/projects/starter/HealthySnacks/Snacks/ViewController.swift
@@ -27,86 +27,110 @@
 /// THE SOFTWARE.
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
-  
-  @IBOutlet var imageView: UIImageView!
-  @IBOutlet var cameraButton: UIButton!
-  @IBOutlet var photoLibraryButton: UIButton!
-  @IBOutlet var resultsView: UIView!
-  @IBOutlet var resultsLabel: UILabel!
-  @IBOutlet var resultsConstraint: NSLayoutConstraint!
-
-  var firstTime = true
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
-    resultsView.alpha = 0
-    resultsLabel.text = "choose or take a photo"
-  }
-
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-
-    // Show the "choose or take a photo" hint when the app is opened.
-    if firstTime {
-      showResultsView(delay: 0.5)
-      firstTime = false
-    }
-  }
-  
-  @IBAction func takePicture() {
-    presentPhotoPicker(sourceType: .camera)
-  }
-
-  @IBAction func choosePhoto() {
-    presentPhotoPicker(sourceType: .photoLibrary)
-  }
-
-  func presentPhotoPicker(sourceType: UIImagePickerController.SourceType) {
-    let picker = UIImagePickerController()
-    picker.delegate = self
-    picker.sourceType = sourceType
-    present(picker, animated: true)
-    hideResultsView()
-  }
-
-  func showResultsView(delay: TimeInterval = 0.1) {
-    resultsConstraint.constant = 100
-    view.layoutIfNeeded()
-
-    UIView.animate(withDuration: 0.5,
-                   delay: delay,
-                   usingSpringWithDamping: 0.6,
-                   initialSpringVelocity: 0.6,
-                   options: .beginFromCurrentState,
-                   animations: {
-      self.resultsView.alpha = 1
-      self.resultsConstraint.constant = -10
-      self.view.layoutIfNeeded()
-    },
-    completion: nil)
-  }
-
-  func hideResultsView() {
-    UIView.animate(withDuration: 0.3) {
-      self.resultsView.alpha = 0
-    }
-  }
-
-  func classify(image: UIImage) {
     
-  }
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var cameraButton: UIButton!
+    @IBOutlet var photoLibraryButton: UIButton!
+    @IBOutlet var resultsView: UIView!
+    @IBOutlet var resultsLabel: UILabel!
+    @IBOutlet var resultsConstraint: NSLayoutConstraint!
+    
+    lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            // 1
+            let healthySnacks = HealthySnacks()
+            
+            // 2
+            let visionModel = try VNCoreMLModel(for: healthySnacks.model)
+            
+            // 3
+            let request = VNCoreMLRequest(model: visionModel,
+                                          completionHandler: {
+                                            [weak self] request, error in
+                                            print("Request is finished!", request.results)
+                                          })
+            // 4
+            request.imageCropAndScaleOption = .centerCrop
+            return request
+        } catch {
+            fatalError("Failed to create VNCoreMLModel: \(error)")
+        }
+    }()
+    
+    var firstTime = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        resultsView.alpha = 0
+        resultsLabel.text = "choose or take a photo"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Show the "choose or take a photo" hint when the app is opened.
+        if firstTime {
+            showResultsView(delay: 0.5)
+            firstTime = false
+        }
+    }
+    
+    @IBAction func takePicture() {
+        presentPhotoPicker(sourceType: .camera)
+    }
+    
+    @IBAction func choosePhoto() {
+        presentPhotoPicker(sourceType: .photoLibrary)
+    }
+    
+    func presentPhotoPicker(sourceType: UIImagePickerController.SourceType) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = sourceType
+        present(picker, animated: true)
+        hideResultsView()
+    }
+    
+    func showResultsView(delay: TimeInterval = 0.1) {
+        resultsConstraint.constant = 100
+        view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: 0.5,
+                       delay: delay,
+                       usingSpringWithDamping: 0.6,
+                       initialSpringVelocity: 0.6,
+                       options: .beginFromCurrentState,
+                       animations: {
+                        self.resultsView.alpha = 1
+                        self.resultsConstraint.constant = -10
+                        self.view.layoutIfNeeded()
+                       },
+                       completion: nil)
+    }
+    
+    func hideResultsView() {
+        UIView.animate(withDuration: 0.3) {
+            self.resultsView.alpha = 0
+        }
+    }
+    
+    func classify(image: UIImage) {
+        
+    }
 }
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-    picker.dismiss(animated: true)
-
-	let image = info[.originalImage] as! UIImage
-    imageView.image = image
-
-    classify(image: image)
-  }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        
+        let image = info[.originalImage] as! UIImage
+        imageView.image = image
+        
+        classify(image: image)
+    }
 }
